873c282d7d3206b986147e2ce103a5309446b81b
Morten Sickel <morten@norsar.no>
2022-10-21 12:09:43 +0200

    Added extractchannels

--- lowportion.py
+++ lowportion.py
@@ -29,3 +29,33 @@ def lowportion(arrayfield, splitch,feature, parent):
     low=sum(spec[0:splitch])
     high=sum(spec[splitch:])
     return low/(high+low)
+
+
+@qgsfunction(args='auto', group='Gamma')
+def extractchannels(arrayfield, fromch, toch, feature, parent):
+    """
+    <h2>Description</h2>
+    Extracts a set of channels from a spectre
+    
+    extract=sum(spectre[fromch:toch])
+    
+    fromch needs to be less than toch
+    
+    Since the last channel is used in RSI spectre to store the cosmic count, this channel 
+    is removed from the spectra before calculation. 
+    
+    
+    
+    <h2>Example usage:</h2>
+    <ul>
+      <li>extractchs(field,fromch,toch) -> integer</li>
+    </ul>
+    
+    """
+    spec=arrayfield
+    if isinstance(spec, str):
+        spec=spec.split(',')
+    del spec[-1] # Cosmic in RSI-spectre
+    spec = list(map(float, spec)) # Converts in case
+    extract = sum(spec[fromch:toch])
+    return extract
